89f0b2a74b93a19b9f30ca85bb04b9b8031177a9
Daniel Hammond <daniel@twotoasters.com>
2012-09-30 09:15:58 -0400

    Initial Commit

---
+++ AppSearcher/ASAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ASAppDelegate.h
+//  AppSearcher
+//
+//  Created by Daniel Hammond on 9/30/12.
+//  Copyright (c) 2012 Two Toasters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ASAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ AppSearcher/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  AppSearcher
+//
+//  Created by Daniel Hammond on 9/30/12.
+//  Copyright (c) 2012 Two Toasters. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ASAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ASAppDelegate class]));
+    }
+}
